66bc3b89b9788e1c596b7ccbde23effd0a7a68c7
ben.galley <benjamin.galley@e2x.com>
2022-12-08 16:05:17 +0000

    implement fixed bounties

---
+++ src/fixedMysteryBounty.js
@@ -0,0 +1,53 @@
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+const revealBounty = async (bounties, player) => {
+  console.log(`${player} wins a mystery bounty of.... \n\n`)
+  await sleep(2000);
+  const bounty = selectMysteryBounty(bounties);
+  console.log(`${bounty} ! \n\n`);
+
+  if (bounty === 0.01) {
+    console.log("Unlucky, you got the TILT stick!")
+  } else {
+    console.log("Nice!")
+  };
+};
+
+const selectMysteryBounty = (bounties) => {
+  const randomIndex = Math.floor(Math.random() * bounties.length);
+
+  return bounties[randomIndex];
+};
+
+const calculateBounties = (entry, players) => {
+  let total = entry * players;
+  const prizes = [];
+  let remainingPlayers = players;
+
+  for (let i = 0; i < players; i++) {
+    let value;
+
+    if (remainingPlayers === players) {
+      value = 0.01
+    } else {
+      value = Math.floor(Math.random() * ((total/remainingPlayers) * 100)) / 100;
+    };
+
+    if (remainingPlayers > 1 ) {
+      remainingPlayers -= 1;
+      console.log(remainingPlayers)
+      prizes.push(Number(value.toFixed(2)));
+      total -= value;
+    } else {
+      prizes.push(Number(total.toFixed(2)));
+    };
+  };
+ 
+  return prizes;
+};
+
+const bountyPool = calculateBounties(5, 10);
+
+revealBounty(bountyPool, "Player Name");
